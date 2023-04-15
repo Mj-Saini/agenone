33e8ff97582eb80179d5ddfbcf4adff3c72260aa
mk0029 <mkhicher404@gmail.com>
2023-04-15 16:09:15 +0530

    slide

--- src/components/GridComponents.jsx
+++ src/components/GridComponents.jsx
@@ -9,11 +9,288 @@ function GridComponents() {
     <>
       <section
         style={{ maxHeight: "800px" }}
-        className=" overflow-auto"
-        id="GridComponents"
-      >
+        className=" overflow-hidden"
+        id="GridComponents">
         <div className=" container">
-          <div className=" d-grid">
+          <div class="LogoParant ">
+            <div class="slide_logo d-flex gap-5 px-3 align-items-center">
+              <div className=" d-grid">
+                <div className="gridbox1 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar1}
+                      alt="Err????
+                   to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox2 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar2}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox3 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar3}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox4 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar4}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox5 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar4}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox6 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar2}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="slide_logo d-flex gap-5  align-items-center ms-auto pt-0">
+              {" "}
+              <div className=" d-grid">
+                <div className="gridbox1 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar1}
+                      alt="Err????
+                   to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox2 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar2}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox3 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar3}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox4 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar4}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox5 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar4}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="gridbox6 rounded-4 bg_lightWhite p-4">
+                  <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    aliquam diam sagittis sagittis ullamcorper. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit. aliquam.
+                  </p>
+                  <div className="d-flex align-items-center gap-3 pt-4">
+                    <img
+                      style={{ width: "72px" }}
+                      src={Avtar2}
+                      alt="Err to load Png "
+                    />
+                    <div>
+                      <h2 className=" text_clr_offBlue ff_inter fs_lg fw-semibold">
+                        Clara Insomnia
+                      </h2>
+                      <h4 className=" text_clr_offBlue ff_inter fs_sm fw-normal">
+                        Occupation
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className=" d-grid">
             <div className="gridbox1 rounded-4 bg_lightWhite p-4">
               <p className=" ff_inter fs_lg fw-normal  text_clr_offBlue">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam
@@ -147,7 +424,7 @@ function GridComponents() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
