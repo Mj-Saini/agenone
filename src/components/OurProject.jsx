e691a037b394b2ce7a170e46793cd458443cf74e
mk0029 <mkhicher404@gmail.com>
2023-04-17 13:59:48 +0530

    UpdateMistakes

--- src/components/OurProject.jsx
+++ src/components/OurProject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BgImgGrid from "../assets/img/png/Grid-lines-bg.png";
 import Circelsmall from "../assets/img/png/Circlesmall.png";
 import CircelBIG from "../assets/img/png/CircleBIG.png";
@@ -14,12 +14,17 @@ import Logo5 from "../assets/img/png/Adobe.png";
 import Logo6 from "../assets/img/png/Microsoft.png";
 
 function OurProjects() {
+
+ const [activeTab, setActiveTab] = useState("Digital");
+ const handleTabClick = (tab) => {
+   setActiveTab(tab);
+ };
+  
   return (
     <>
       <section
         className="bg_img_Ourprojects bg_primery position-relative min-vh-100 "
-        id="OurProjects"
-      >
+        id="OurProjects">
         <img
           src={BgImgGrid}
           className=" position-absolute w-100 h-100 z_index0"
@@ -39,35 +44,46 @@ function OurProjects() {
           <div
             data-aos="fade-right"
             data-aos-easing="ease-in"
-            data-aos-delay="300"
-            data-aos-duration="2000"
-            className="py-md-5 "
-          >
+            data-aos-delay="0"
+            data-aos-duration="1000"
+            className="py-md-5 ">
             {" "}
             <h2 className=" text-center ff_intes fs_7xl fw-bold text_clr_white">
               Our Project Have Been <span className="text_Grident">Done</span>{" "}
             </h2>
             <p
               style={{ maxWidth: "928px" }}
-              className="text-center ff_inter fs_lg fw-med mx-auto text_clr_dimGrey pt-3"
-            >
+              className="text-center ff_inter fs_lg fw-med mx-auto text_clr_dimGrey pt-3">
               Take a look at some of our recent projects and see how we've
               helped other businesses elevate their brand and drive business
               growth.
             </p>
             <div className="d-flex justify-content-center mt-5 gap-sm-2 pt-3">
               <div className=" text_clr_offBlue fw-med fs_sm ff_inter  bg-white d-inline-block p-1 p-sm-2 rounded-100 ">
-                <span className="btn_our_info cursor_pointer d-inline-block ">
-                  UI/UX <span className="d-none d-sm-inline-block">Design</span>
+                <span
+                  className={activeTab ? "Digital cursor_pointer" : "active"}
+                  onClick={() => handleTabClick("Digital")}>
+                  <span className="btn_our_info cursor_pointer d-inline-block">UI/UX
+                    <span className="d-none d-sm-inline-block">   Design </span>{" "}
+                 
+                  </span>
                 </span>
-                <span className="btn_our_info cursor_pointer d-inline-block">
-                  <span className="d-none d-d-sm-inline-block"> Digital</span>{" "}
-                  Marketing
+                <span
+                  className={activeTab ? "Digital cursor_pointer" : "active"}
+                  onClick={() => handleTabClick("Digital")}>
+                  <span className="btn_our_info cursor_pointer d-inline-block">
+                    <span className="d-none d-d-sm-inline-block"> Digital</span>{" "}
+                    Marketing
+                  </span>
                 </span>
-                <span className="btn_our_info cursor_pointer d-inline-block">
-                  <span className="d-none d-lg-inline-block"> Sosmed</span>{" "}
-                  <span className="d-none d-sm-inline-block">Management</span>{" "}
-                  <span className=" d-sm-none">SMM</span>
+                <span
+                  className={activeTab ? "uiux cursor_pointer" : "active"}
+                  onClick={() => handleTabClick("uiux")}>
+                  <span className="btn_our_info cursor_pointer d-inline-block">
+                    <span className="d-none d-lg-inline-block"> Sosmed</span>{" "}
+                    <span className="d-none d-sm-inline-block">Management</span>{" "}
+                    <span className=" d-sm-none">SMM</span>
+                  </span>
                 </span>
                 <span className="btn_our_info cursor_pointer d-inline-block">
                   Dev<span className="d-none d-md-inline-block">elopment</span>
@@ -76,84 +92,162 @@ function OurProjects() {
             </div>
             <div className=" pt-4 mt-5 pb-5">
               <div className="row gap-lg-0 gap-sm-4 gap-2">
-                <div
-                  data-aos="fade-right"
-                  data-aos-easing="ease-in"
-                  data-aos-delay="300"
-                  data-aos-duration="2000"
-                  className="col-lg-6 "
-                >
-                  <div className=" bg_blue rounded-4 p-3 overlayParant">
-                    <div className=" position-relative">
-                      <div className=" position-absolute  w-100 h-100 overlay transition300">
-                        <img
-                          className=" w-100 h-100 position-absolute"
-                          src={bg_layer_blur}
-                          alt="Error to Fatch Api"
-                        />
-                        <div className=" position-absolute CircleBgArrow d-inline-block">
+                {activeTab === "Digital" && (
+                  <div
+                    data-aos="fade-right"
+                    data-aos-easing="ease-in"
+                    data-aos-delay="0"
+                    data-aos-duration="1000"
+                    className="col-lg-6 ">
+                    <div className=" bg_blue rounded-4 p-3 overlayParant">
+                      <div className=" position-relative">
+                        <div className=" position-absolute  w-100 h-100 overlay transition300">
                           <img
-                            className="transition300 cursor_pointer"
-                            style={{ width: "218px" }}
-                            src={Circle_Arrow}
+                            className=" w-100 h-100 position-absolute"
+                            src={bg_layer_blur}
                             alt="Error to Fatch Api"
                           />
+                          <div className=" position-absolute CircleBgArrow d-inline-block">
+                            <img
+                              className="transition300 cursor_pointer img_circle"
+                              src={Circle_Arrow}
+                              alt="Error to Fatch Api"
+                            />
+                          </div>
                         </div>
-                      </div>
-                      <img
-                        className="w-100 "
-                        src={img_ourProjects_col6_1}
-                        alt="Error to Fatch Api"
-                      />
-                    </div>
-                    <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
-                      Title
-                    </h2>
-                    <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
-                      We are designed to help your business thrive in the online
-                      world.
-                    </p>
-                  </div>
-                </div>
-                <div
-                  data-aos="fade-left"
-                  data-aos-easing="ease-in"
-                  data-aos-delay="300"
-                  data-aos-duration="2000"
-                  className="col-lg-6 "
-                >
-                  <div className=" bg_blue rounded-4 p-3 overlayParant">
-                    <div className=" position-relative">
-                      <div className=" position-absolute  w-100 h-100 overlay transition300">
                         <img
-                          className=" w-100 h-100 position-absolute"
-                          src={bg_layer_blur}
+                          className="w-100 "
+                          src={img_ourProjects_col6_1}
                           alt="Error to Fatch Api"
                         />
-                        <div className=" position-absolute CircleBgArrow d-inline-block">
+                      </div>
+                      <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
+                        Title
+                      </h2>
+                      <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
+                        We are designed to help your business thrive in the
+                        online world.
+                      </p>
+                    </div>
+                  </div>
+                )}
+                {activeTab === "Digital" && (
+                  <div
+                    data-aos="fade-left"
+                    data-aos-easing="ease-in"
+                    data-aos-delay="0"
+                    data-aos-duration="1000"
+                    className="col-lg-6 ">
+                    <div className=" bg_blue rounded-4 p-3 overlayParant">
+                      <div className=" position-relative">
+                        <div className=" position-absolute  w-100 h-100 overlay transition300">
                           <img
-                            className="transition300 cursor_pointer"
-                            style={{ width: "218px" }}
-                            src={Circle_Arrow}
+                            className=" w-100 h-100 position-absolute"
+                            src={bg_layer_blur}
                             alt="Error to Fatch Api"
                           />
+                          <div className=" position-absolute CircleBgArrow d-inline-block">
+                            <img
+                              className="transition300 cursor_pointer img_circle"
+                              src={Circle_Arrow}
+                              alt="Error to Fatch Api"
+                            />
+                          </div>
                         </div>
+                        <img
+                          className="w-100 rounded-5 "
+                          src={img_ourProjects_col6_2}
+                          alt="Error to Fatch Api"
+                        />
                       </div>
-                      <img
-                        className="w-100 rounded-5 "
-                        src={img_ourProjects_col6_2}
-                        alt="Error to Fatch Api"
-                      />
+                      <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
+                        Title
+                      </h2>
+                      <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
+                        We are designed to help your business thrive in the
+                        online world.
+                      </p>
                     </div>
-                    <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
-                      Title
-                    </h2>
-                    <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
-                      We are designed to help your business thrive in the online
-                      world.
-                    </p>
                   </div>
-                </div>
+                )}
+                {activeTab === "uiux" && (
+                  <div
+                    data-aos="fade-left"
+                    data-aos-easing="ease-in"
+                    data-aos-delay="0"
+                    data-aos-duration="1000"
+                    className="col-lg-6 ">
+                    <div className=" bg_blue rounded-4 p-3 overlayParant">
+                      <div className=" position-relative">
+                        <div className=" position-absolute  w-100 h-100 overlay transition300">
+                          <img
+                            className=" w-100 h-100 position-absolute"
+                            src={bg_layer_blur}
+                            alt="Error to Fatch Api"
+                          />
+                          <div className=" position-absolute CircleBgArrow d-inline-block">
+                            <img
+                              className="transition300 cursor_pointer img_circle"
+                              src={Circle_Arrow}
+                              alt="Error to Fatch Api"
+                            />
+                          </div>
+                        </div>
+                        <img
+                          className="w-100 rounded-5 "
+                          src={img_ourProjects_col6_2}
+                          alt="Error to Fatch Api"
+                        />
+                      </div>
+                      <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
+                        Title
+                      </h2>
+                      <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
+                        We are designed to help your business thrive in the
+                        online world.
+                      </p>
+                    </div>
+                  </div>
+                )}
+                {activeTab === "uiux" && (
+                  <div
+                    data-aos="fade-right"
+                    data-aos-easing="ease-in"
+                    data-aos-delay="0"
+                    data-aos-duration="1000"
+                    className="col-lg-6 ">
+                    <div className=" bg_blue rounded-4 p-3 overlayParant">
+                      <div className=" position-relative">
+                        <div className=" position-absolute  w-100 h-100 overlay transition300">
+                          <img
+                            className=" w-100 h-100 position-absolute"
+                            src={bg_layer_blur}
+                            alt="Error to Fatch Api"
+                          />
+                          <div className=" position-absolute CircleBgArrow d-inline-block">
+                            <img
+                              className="transition300 cursor_pointer img_circle"
+                              src={Circle_Arrow}
+                              alt="Error to Fatch Api"
+                            />
+                          </div>
+                        </div>
+                        <img
+                          className="w-100 "
+                          src={img_ourProjects_col6_1}
+                          alt="Error to Fatch Api"
+                        />
+                      </div>
+                      <h2 className=" text_clr_white pt-2 mt-3 ff_inter fw-bold fs_3xl">
+                        Title
+                      </h2>
+                      <p className=" ff_inter fw-normal fs_lg  text_clr_dimGrey pt-2 mt-1">
+                        We are designed to help your business thrive in the
+                        online world.
+                      </p>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -162,11 +256,10 @@ function OurProjects() {
       <section
         data-aos="fade-down"
         data-aos-easing="ease-in"
-        data-aos-delay="300"
-        data-aos-duration="2000"
+        data-aos-delay="0"
+        data-aos-duration="1000"
         className=" bg_primery "
-        id="LogoParant"
-      >
+        id="LogoParant">
         <div className=" container ">
           <div class="LogoParant ">
             <div class="slide_logo d-flex gap-5 px-3 align-items-center">
